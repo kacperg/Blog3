3ea0338b86c1781f061dbbf53bea4d6b7d1ee303
kacperg <teng@o2.pl>
2016-05-24 21:52:53 +0200

    update

--- Blog.DAL.Tests/RepositoryTests.cs
+++ Blog.DAL.Tests/RepositoryTests.cs
@@ -12,7 +12,6 @@ using TDD.DbTestHelpers.Yaml;
 using TDD.DbTestHelpers.Core;
 using System.Collections.Generic;
 
-
 namespace Blog.DAL.Tests
 {
 
@@ -33,39 +32,9 @@ namespace Blog.DAL.Tests
     public class RepositoryTests
     {
 
-        /*public class Comment
-        {
-            public long Id { get; set; }
-
-            public string Content { get; set; }
-
-            public long Post { get; set; }
-        }
-
-        public class Post
-        {
-            public long Id { get; set; }
-
-            public long Nr { get; set; }
-
-            public string Content { get; set; }
-
-            public string Author { get; set; }
-        }*/
-
         public class DbBaseTest<BlogFixtures>
         {
-            
-        }
 
-        [TestMethod]
-        public void OneShouldBeOne()
-        {
-            // arrange
-            // act
-            int i = 1;
-            // assert
-            Assert.AreEqual(1, i);
         }
 
         [TestMethod]
@@ -74,50 +43,7 @@ namespace Blog.DAL.Tests
             // arrange
             List<Post> posty = new List<Post>();
             List<Comment> komentarze = new List<Comment>();
-            var post = new Post();
-
-            post.Id = 1;
-            post.Nr = 1;
-            post.Author = "autor 1";
-            post.Content = "post 1";
-            posty.Add(post);
-
-            var post2 = new Post();
-
-            post2.Id = 2;
-            post2.Nr = 2;
-            post2.Author = "autor 2";
-            post2.Content = "post 2";
-            posty.Add(post2);
-
-            var post3 = new Post();
-
-            post3.Id = 3;
-            post3.Nr = 3;
-            post3.Author = "autor 2";
-            post3.Content = "post 3";
-            posty.Add(post3);
-
-            var komentarz = new Comment();
-
-            komentarz.Id = 1;
-            komentarz.Post = 1;
-            komentarz.Content = "komentarz do postu 1";
-            komentarze.Add(komentarz);
-
-            var komentarz2 = new Comment();
-
-            komentarz2.Id = 2;
-            komentarz2.Post = 1;
-            komentarz2.Content = "drugi komentarz do postu 1";
-            komentarze.Add(komentarz2);
-
-            var komentarz3 = new Comment();
-
-            komentarz3.Id = 3;
-            komentarz3.Post = 2;
-            komentarz3.Content = "komentarz do postu 2";
-            komentarze.Add(komentarz3);
+            Setup(posty, komentarze);
 
             // act
             int ile = posty.Count();
@@ -125,57 +51,42 @@ namespace Blog.DAL.Tests
             Assert.AreEqual(3, ile);
         }
 
-        /*
-        [TestMethod]
-        public void GetAllPost_ThreePostsInDb_ReturnThreePosts()
-        {
-            // arrange
-            var repository = new BlogRepository();
-            Setup();
-
-            // act
-            var result = repository.GetAllPosts();
-            // assert
-            Assert.AreEqual(3, result.Count());
-        }
-
         [TestMethod]
         public void GetPostsByAutor2_WroteTwoPosts_ReturnCorrectPostNames()
         {
             // arrange
-            var repository = new BlogRepository();
-            Setup();
+            List<Post> posty = new List<Post>();
+            List<Comment> komentarze = new List<Comment>();
+            Setup(posty, komentarze);
 
             // act
-            var results = repository.GetAllPosts();
             string contents;
             contents = null;
-            foreach(var result in results)
+            for (int i = 0; i < posty.Count; i++)
             {
-                if (result.Author == "autor 2")
+                if (posty[i].Author == "autor 2")
                 {
-                    contents += result.Content;
+                    contents += posty[i].Content;
                     contents += ", ";
                 }
             }
             // assert
-            Assert.AreEqual(contents,"post 2, post 3, ");
+            Assert.AreEqual(contents, "post 2, post 3, ");
         }
 
         [TestMethod]
         public void GetCommentsToPostOne_HasTwoComments_ReturnTwoComments()
         {
             // arrange
-            var repository = new BlogRepository();
-            Setup();
+            List<Post> posty = new List<Post>();
+            List<Comment> komentarze = new List<Comment>();
+            Setup(posty, komentarze);
 
             // act
-            var context = new BlogContext();
-            var comments = context.Comments.ToList();
             int num = 0;
-            foreach (var comment in comments)
+            for (int i = 0; i < komentarze.Count; i++)
             {
-                if (comment.Post == 1)
+                if (komentarze[i].Post == 1)
                     num++;
             }
             // assert
@@ -186,25 +97,25 @@ namespace Blog.DAL.Tests
         public void GetCommentsToPostTwo_HasOneComment_ReturnCorrectCommentContent()
         {
             // arrange
-            var repository = new BlogRepository();
-            Setup();
+            List<Post> posty = new List<Post>();
+            List<Comment> komentarze = new List<Comment>();
+            Setup(posty, komentarze);
 
             // act
             var context = new BlogContext();
-            var comments = context.Comments.ToList();
             string contents;
             contents = null;
-            foreach (var comment in comments)
+            for (int i = 0; i < komentarze.Count; i++)
             {
-                if (comment.Post == 2)
+                if (komentarze[i].Post == 2)
                 {
-                    contents += comment.Content;
+                    contents += komentarze[i].Content;
                     contents += ", ";
                 }
             }
             // assert
             Assert.AreEqual(contents, "komentarz do postu 2, ");
-        }*/
+        }
 
         public void Setup(List<Post> posty, List<Comment> komentarze)
         {
